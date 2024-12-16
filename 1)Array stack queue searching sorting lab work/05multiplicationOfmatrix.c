b9a51bdd369e5e49ef6680031ccae05263c2b624
Arka Garai <arka292@gmail.com>
2024-12-16 17:47:33 +0530

    c

--- 1)Array stack queue searching sorting lab work/05multiplicationOfmatrix.c
+++ 1)Array stack queue searching sorting lab work/05multiplicationOfmatrix.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main() {
     int m,n,p,q,i,j,k;
-    printf("Enter the number of rows of matrix1: ");
+    printf("Enter the number of rows for matrix1: ");
     scanf("%d", &m);
-    printf("Enter the number of columns of matrix1: ");
+    printf("Enter the number of columns for matrix1: ");
     scanf("%d", &n);
-    printf("Enter the number of rows of matrix2: ");
+    printf("Enter the number of rows for matrix2: ");
     scanf("%d", &p);
-    printf("Enter the number of columns  of matrix2: ");
+    printf("Enter the number of columns  for matrix2: ");
     scanf("%d", &q);
 
     int arr1[m][n], arr2[p][q], mul[m][q]; 
 
-    printf("Enter the elements of matrix1:\n");
+    printf("Enter the elements for matrix1:\n");
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
             scanf("%d", &arr1[i][j]);
         }
     }
-    printf("Enter the elements of matrix2:\n");
+    printf("Enter the elements for matrix2:\n");
     for (i = 0; i < p; i++) {
         for (j = 0; j < q; j++) {
             scanf("%d", &arr2[i][j]);
