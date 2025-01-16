bc883c523318f7d6b1972b8e564ab8dd61474642
Arka Garai <arka292@gmail.com>
2025-01-17 00:42:28 +0530

    c

--- 1)Array Stack Recursion Queue Sorting/07Infix_to_postfix.c
+++ 1)Array Stack Recursion Queue Sorting/07Infix_to_postfix.c
@@ -1,93 +1,88 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
 #define MAX 100
-
 char stack[MAX];
 int top = -1;
 
-
-void push(char c) {
-    if (top == MAX - 1) {
-        printf("Stack Overflow\n");
-    } else {
-        stack[++top] = c;
-    }
+void push(char ch){
+	if(top == MAX-1){
+		printf("Stack Overflow");
+		exit(1);
+	}
+	else{
+		stack[++top]=ch;
+	}
 }
 
-
-char pop() {
-    if (top == -1) {
-        printf("Stack Underflow\n");
-        return -1;
-    } else {
-        return stack[top--];
-    }
+char pop(){
+	if(top == -1){
+		printf("Stack Underflow");
+		exit(1);
+	}
+	else{
+		return stack[top--];
+	}
 }
 
-
-int precedence(char c) {
-    if (c == '^')
-        return 3;
-    else if (c == '*' || c == '/')
-        return 2;
-    else if (c == '+' || c == '-')
-        return 1;
-    else
-        return 0;
+int isoperator(char ch){
+	return (ch== '+'||ch== '-'||ch== '/'||ch== '%'||ch== '^'||ch== '*');
+	
 }
 
-
-int isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+int precedence (char ch){
+	if(ch== "^") return 3;
+	else if(ch== '/'||ch== '%'||ch== '*') return 2;
+	else if(ch== '+'||ch== '-') return 1;
+	else return 0;
 }
 
-
-void infixToPostfix(char infix[], char postfix[]) {
-    int i, j = 0;
-    char c;
-
-    for (i = 0; infix[i] != '\0'; i++) {
-        // If the character is an operand, add it to the output
-        if (isalnum(infix[i])) {
-            postfix[j++] = infix[i];
-        }
-        // If the character is '(', push it to the stack
-        else if (infix[i] == '(') {
-            push(infix[i]);
-        }
-        // If the character is ')', pop and output from the stack until '(' is found
-        else if (infix[i] == ')') {
-            while (top != -1 && (c = pop()) != '(') {
-                postfix[j++] = c;
-            }
-        }
-        // If an operator is encountered
-        else if (isOperator(infix[i])) {
-            while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) {
-                postfix[j++] = pop();
-            }
-            push(infix[i]);
-        }
-    }
-
-    // Pop all the operators from the stack
-    while (top != -1) {
-        postfix[j++] = pop();
-    }
-
-    postfix[j] = '\0';  // Null-terminate the postfix expression
+void infixToPostfix(char infix[],char postfix[]){
+	int i,j=0;
+	char ch;
+	for(i=0; infix[i]!='\0';i++){
+		//operand
+		if(isalnum(infix[i])){
+		   postfix[j]=infix[i];
+		   j++;	
+		}
+		//'('
+		else if(infix[i]=='('){
+			push(infix[i]);
+		}
+		//')'
+		else if(infix[i]==')'){
+		    while( top!=-1 && stack[top] !='(' ){
+			   postfix[j]=pop();
+			   j++;
+		    }
+		    pop();
+		}
+		
+		//operator
+		else if( isoperator(infix[i])){
+			while(top!=-1 && precedence(stack[top])>=precedence(infix[i])){
+				postfix[j] = pop();
+				j++;
+			}
+			push(infix[i]);
+		}
+		
+	}
+		while(top!=-1){
+        postfix[j] = pop();
+		j++;
+	    }	
+	    
+	    postfix[j]='\0';
+		
 }
 
-int main() {
-    char infix[MAX], postfix[MAX];
-
-    printf("Enter infix expression: ");
-    scanf("%s", infix);
-    infixToPostfix(infix, postfix);
-
-    printf("Postfix expression: %s\n", postfix);
-
-    return 0;
+int main(){
+	char infix[MAX],postfix[MAX];
+	printf("Enter Infix String: ");
+	scanf("%s",infix);
+	infixToPostfix(infix,postfix);
+	printf("Postfi Expression is: %s",postfix);
+	return 0;
 }
