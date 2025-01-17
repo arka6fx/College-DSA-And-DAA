703549624ca5ed01f2c85a0623024a5a480cd32c
Arka Garai <arka292@gmail.com>
2025-01-17 12:10:41 +0530

    c

--- 4)Extra Problems/03CountFreq.c
+++ 4)Extra Problems/03CountFreq.c
@@ -1,53 +1,50 @@
 #include <stdio.h>
-#define N 50
+
 int main()
 {
-    int arr[N], brr[N], n, i, j, c;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    printf("Enter the elements of the array: ");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    for (i = 0; i < n; i++)
-    {
-        c = 1;
-        if (arr[i] != -1)
-        {
-            for (j = i + 1; j < n; j++)
-            {
-                if (arr[i] == arr[j])
-                {
-                    c++;
-                    arr[j] = -1;
-                }
-            }
-        }
-        brr[i] = c; // stores frequency
-    }
-    for (i = 0; i < n; i++)
-    {
-        if (arr[i] != -1)
-        {
-            printf("no of %d is %d\n", arr[i], brr[i]);
-        }
-    }
+   int arr[50],crr[50],n,i,j,cnt;
+   printf("Enter the no. of Elements: ");
+   scanf("%d",&n);
+   printf("Enter the Elements: ");
+   for(i=0;i<n;i++){
+      scanf("%d", &arr[i]);
+   }
+   
+   for(i=0;i<n;i++){
+       cnt=1;
+       if(arr[i]!=-1){
+           for(j=i+1;j<n;j++){
+               if(arr[i]==arr[j]){
+                   arr[j]=-1;
+                   cnt++;
+               }
+           }
+       }
+       crr[i]=cnt;
+   }
+   
+   for(i=0;i<n;i++){
+       if(arr[i]!=-1){
+           printf("Frequency of %d is %d\n",arr[i],crr[i]);
+       }
+   }
+   
     printf("\nDuplicate elements: \n");
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] != -1 && brr[i] > 1)
+        if (arr[i] != -1 && crr[i] > 1)
         {
-            printf("%d->%d\n", arr[i], brr[i]);
+            printf("%d->%d\n", arr[i], crr[i]);
         }
     }
     printf("Unique elements: \n");
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] != -1 && brr[i] == 1)
+        if (arr[i] != -1 && crr[i] == 1)
         {
-            printf("%d->%d\n", arr[i], brr[i]);
+            printf("%d->%d\n", arr[i], crr[i]);
         }
     }
-    return 0;
+
+   return 0;
 }
