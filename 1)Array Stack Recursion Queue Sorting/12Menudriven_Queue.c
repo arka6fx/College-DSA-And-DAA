16ce5a5b227c9926487d6ffdadbae43cbd7001c7
Arka Garai <arka292@gmail.com>
2025-01-17 11:39:58 +0530

    c

--- 1)Array Stack Recursion Queue Sorting/12Menudriven_Queue.c
+++ 1)Array Stack Recursion Queue Sorting/12Menudriven_Queue.c
@@ -1,64 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
 #define MAX 5
 int queue[MAX];
-int rear = -1;
-int front = -1;
+int front=-1;
+int rear=-1;
 
-void enqueue(int val)
-{
-   if (rear == MAX - 1)
-   {
-      printf("Overflow");
-      return;
-   }
-   else if (rear == -1 && front == -1)
-   {
-      rear = front = 0;
-   }
-   else
-   {
-      rear++;
-   }
-   queue[rear] = val;
+void enqueue(int data){
+	if(rear == MAX-1){
+		printf("Queue Overflow");
+	}
+	else if(rear == -1 && front == -1){
+		rear=front=0;
+		queue[rear]=data;
+	}
+	else{
+		rear=rear+1;
+		queue[rear]=data;
+	}
 }
 
-int dequeue()
-{
-   int data;
-   if (rear == -1 && front == -1)
-   {
-      printf("Underflow");
-      exit(1);
-   }
-   else if (rear == front)
-   {
-      data = queue[front];
-      rear = front = -1;
-   }
-   else
-   {
-      data = queue[front];
-      front=front+1;
-   }
-   return data;
-
+int dequeue(){
+	int data;
+	if( rear==-1 && front==-1){
+		printf("Queue Underflow");
+		exit(1);
+	}
+	else if( rear==front ){
+		data=queue[front];
+		rear=front=-1;
+	}
+	else{
+		data=queue[front];
+		front=front+1;
+	}
+	
+	return data;
 }
 
-void display()
-{
-   int i;
-   if (rear == -1 && front == -1)
-   {
-      printf("Underflow");
-      return;
-   }
-   printf("Your Queue elements are: \n");
-   for (i = front; i < rear + 1; i++)
-   {
-      printf("%d ", queue[i]);
-   }
+void display(){
+	if( rear==-1 && front==-1){
+		printf("Queue Underflow");
+		exit(1);
+	}
+	int i;
+	printf("\n---Queue---\n");
+	for(i=front;i<=rear;i++){
+		printf("%d ",queue[i]);
+	}
+	
 }
+
+void IsFull(){
+	if(rear == MAX-1){
+		printf("True");
+	}
+}
+
+void IsEmpty(){
+	if( rear==-1 && front==-1){
+		printf("True");
+	}
+}
+
 int main()
 {
    int choice, val;
@@ -68,7 +72,9 @@ int main()
       printf("1.Insert\n");
       printf("2.Delete\n");
       printf("3.Display\n");
-      printf("4.Exit\n");
+      printf("4.Is Queue Empty?\n");
+      printf("5.Is Queue Full?\n");
+      printf("6.Exit\n");
       printf("Enter your choice: ");
       scanf("%d", &choice);
       switch (choice)
@@ -85,7 +91,13 @@ int main()
       case 3:
          display();
          break;
-      case 4:
+      case 4: 
+	     IsEmpty();
+		 break;   
+	  case 5: 
+	     IsFull();
+		 break; 	 
+      case 6:
          printf("Exiting...");
          exit(0);
       default:
