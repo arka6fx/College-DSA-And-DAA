292326ba77f7eda5b9acf3f8f724393296fcaf9f
Arka Garai <arka292@gmail.com>
2025-01-17 18:39:17 +0530

    c

--- 1)Array Stack Recursion Queue Sorting/11Searching_menudriven.c
+++ 1)Array Stack Recursion Queue Sorting/11Searching_menudriven.c
@@ -1,66 +1,70 @@
 #include <stdio.h>
+#include<stdlib.h>
 
-void linearSearch(int arr[], int n) {
-    int x;
-    printf("Enter the element to search: ");
-        scanf("%d", &x);
-    for(int i = 0; i < n; i++) {
-        if(arr[i] == x) {
-            printf("Element %d found at index %d\n", x, i);
-            return;
-        }
-    }
-    printf("Element %d not found in the array\n", x);
+int linearSearch(int arr[],int n,int data){
+	int i,idx;
+	for(i=0;i<=n-1;i++){
+		if(arr[i] == data){
+			idx=i;
+		    return idx;
+		}	
+	}
+	printf("Element %d not found in the array\n", data);
+	exit(1);
+}
+void bubbleSort(int arr[],int n){
+	int flag,temp,i,j;
+	for(i=1;i<=n-1;i++){
+  		flag=0;//marks the pass where there is no swapping
+  		for(j=1;j<=n-i;j++){
+  			if(arr[j-1]>arr[j]){
+  				temp = arr[j-1];
+  				arr[j-1] = arr[j];
+  				arr[j] = temp;
+  				flag=1;
+			  }
+			  
+		  }
+		  if(flag==0){
+		  	break;
+		  }
+  		
+	}
 }
 
-void binarySearch(int arr[], int n) {
-    int x;
-    printf("Enter the element to search: ");
-        scanf("%d", &x);
-    int beg = 0, end = n - 1;
-    while(beg <= end) {
-        int mid = (beg + end) / 2;
-        if(arr[mid] == x) {
-            printf("Element %d found at index %d\n", x, mid);
-            return;
-        }
-        if(arr[mid] < x) {
-            beg = mid + 1;
-        } else {
-            end = mid - 1;
-        }
-    }
-    printf("Element %d not found in the array\n", x);
+int binarySearch(int arr[],int n,int data){
+	bubbleSort(arr,n);
+	int start=0,end=n-1,idx,mid;
+	while(start<=end){
+		mid = start+(end-start)/2;
+		if(arr[mid]==data){
+			idx=mid;
+		    return idx;
+		}
+		else if(arr[mid]<data){
+			start=mid+1;
+		}
+		else end=mid-1;
+	}
+	printf("Element %d not found in the array\n", data);
+	exit(1);
 }
 
-void sortArray(int arr[], int n) {
-    for(int i = 0; i < n-1; i++) {
-        for(int j = 0; j < n-i-1; j++) {
-            if(arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            } 
-        }
-
-    }
-    printf("Array sorted for binary search.\n");
-}
 
 int main() {
-    int choice, n;
-    printf("Enter the number of elements in the array: ");
+    int choice, n,idx,data,i;
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
     
     int arr[n];
-    printf("Enter the elements of the array:\n");
-    for(int i = 0; i < n; i++) {
+    printf("Enter the elements of the array: ");
+    for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     
     while(1) {
         
-        printf("Choose the search method:\n");
+        printf("\nChoose the search method:\n");
         printf("1. Linear Search\n");
         printf("2. Binary Search\n");
         printf("3. Exit\n");
@@ -68,14 +72,20 @@ int main() {
 
         switch(choice) {
             case 1:
-                linearSearch(arr, n);
+            	printf("Enter the Element to Find: ");
+	            scanf("%d",&data);
+                idx=linearSearch(arr, n,data);
+                printf("%d is present at index %d",data,idx);                
                 break;
             case 2:
-                sortArray(arr, n);
-                binarySearch(arr, n);
+            	printf("Enter the Element to Find: ");
+	            scanf("%d",&data);
+                idx=binarySearch(arr, n,data);
+                printf("%d is present at index %d",data,idx);                
                 break;
-            case 3:
-                return 0;
+            case 3: 
+			    printf("Exiting...");
+                exit(0);
             default:
                 printf("Invalid choice!\n");
         }
