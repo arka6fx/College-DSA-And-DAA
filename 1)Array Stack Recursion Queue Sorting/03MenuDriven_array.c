68a4880d1691263be9707c100f75e85d7287e475
Arka Garai <arka292@gmail.com>
2025-01-20 18:45:44 +0530

    c

--- 1)Array Stack Recursion Queue Sorting/03MenuDriven_array.c
+++ 1)Array Stack Recursion Queue Sorting/03MenuDriven_array.c
@@ -2,78 +2,74 @@
 #include <stdlib.h>
 #define N 50
 
-int insert(int arr[],int n){
-	int data,pos,i;
+int insert(int arr[], int n){
+	int data, pos, i;
 	printf("Enter the Data: ");
-	scanf("%d",&data);
+	scanf("%d", &data);
 	printf("Enter the Position: ");
-	scanf("%d",&pos);
-	for(i=n;i>pos;i--){
-		arr[i]=arr[i-1];
+	scanf("%d", &pos);
+	for (i = n; i > pos; i--){
+		arr[i] = arr[i - 1];
 	}
-	arr[pos]=data;
+	arr[pos] = data;
 	n++;
-	return n;	
+	return n;
 }
 
-int delete(int arr[],int n ){
-	int pos,i;
+int delete(int arr[], int n){
+	int pos, i;
 	printf("Enter the Position:");
-	scanf("%d",&pos);
-	for(i=pos;i<n-1;i++){
-		arr[i]=arr[i+1];
+	scanf("%d", &pos);
+	for (i = pos; i < n - 1; i++){
+		arr[i] = arr[i + 1];
 	}
 	n--;
 	return n;
 }
 
-void display(int arr[],int n){
+void display(int arr[], int n){
 	int i;
-	for(i=0;i<n;i++){
-		printf("%d ",arr[i]);
+	for (i = 0; i < n; i++){
+		printf("%d ", arr[i]);
 	}
 }
 
 int main()
 {
-    int arr[N],choice,n,i;
-    printf("\n-----Array for Organizing & Managing Data-----\n");
-    printf("Enter the no. of elements: ");
-    scanf("%d",&n);
-    printf("Enter the elements: ");
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    while(1){
-        printf("\nMenu: \n");
-        printf("1. Insert an Element\n");
-        printf("2. Delete an Element\n");
-        printf("3. Display the Array\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d",&choice);
-        
-        switch(choice){
-        	case 1: n = insert(arr,n);       	        
-        	        break;
-        	case 2: n = delete(arr,n);
-			        break;
-			case 3: display(arr,n);
-			        break;
-			case 4: printf("Quitting");
-			        exit(0);
-			default: printf("Invalid Choice! Try again ");				        
-            
-        }
-        
-       
-    }
-    
-    
-    
-    
-    
-    
+	int arr[N], choice, n, i;
+	printf("\n-----Array for Organizing & Managing Data-----\n");
+	printf("Enter the no. of elements: ");
+	scanf("%d", &n);
+	printf("Enter the elements: ");
+	for (i = 0; i < n; i++){
+		scanf("%d", &arr[i]);
+	}
+	while (1){
+		printf("\nMenu: \n");
+		printf("1. Insert an Element\n");
+		printf("2. Delete an Element\n");
+		printf("3. Display the Array\n");
+		printf("4. Exit\n");
+		printf("Enter your choice: ");
+		scanf("%d", &choice);
 
-    return 0;
+		switch (choice){
+		case 1:
+			n = insert(arr, n);
+			break;
+		case 2:
+			n = delete (arr, n);
+			break;
+		case 3:
+			display(arr, n);
+			break;
+		case 4:
+			printf("Quitting");
+			exit(0);
+		default:
+			printf("Invalid Choice! Try again ");
+		}
+	}
+
+	return 0;
 }
--- 1)Array Stack Recursion Queue Sorting/04TransposeOfaMatrix.c
+++ 1)Array Stack Recursion Queue Sorting/04TransposeOfaMatrix.c
@@ -1,48 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(){
+	int m, n, i, j;
+	printf("Enter the no. of Rows: ");
+	scanf("%d", &m);
+	printf("Enter the no. of Columns: ");
+	scanf("%d", &n);
+	int arr[m][n], trans[n][m];
 
-int main()
-{
-    int m,n,i,j;
-    printf("Enter the no. of Rows: ");
-    scanf("%d", &m);
-    printf("Enter the no. of Columns: ");
-    scanf("%d", &n);
-    int arr[m][n],trans[n][m];
-    
-    printf("Enter the Elements: ");
-    for(i=0;i<m;i++){
-    	for(j=0;j<n;j++){	
-        scanf("%d",&arr[i][j]);
+	printf("Enter the Elements: ");
+	for (i = 0; i < m; i++){
+		for (j = 0; j < n; j++){
+			scanf("%d", &arr[i][j]);
 		}
 	}
-    
-    for(i=0;i<n;i++){
-    	for(j=0;j<m;j++){
-    		trans[i][j]=arr[j][i];
+
+	for (i = 0; i < n; i++){
+		for (j = 0; j < m; j++){
+			trans[i][j] = arr[j][i];
 		}
 	}
-	
+
 	printf("\nOriginal Matrix\n");
-	for(i=0;i<m;i++){
-    	for(j=0;j<n;j++){
-    		printf("%d ",arr[i][j]);
+	for (i = 0; i < m; i++){
+		for (j = 0; j < n; j++){
+			printf("%d ", arr[i][j]);
 		}
 		printf("\n");
 	}
-	
-	printf("\nTranspose Matrix\n");
-	for(i=0;i<n;i++){
-    	for(j=0;j<m;j++){
-    		printf("%d ",trans[i][j]);
-		}
-		printf("\n");
-	}
-    
-    
-    
-    
 
-    return 0;
+	printf("\nTranspose Matrix\n");
+	for (i = 0; i < n; i++){
+		for (j = 0; j < m; j++){
+			printf("%d ", trans[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
