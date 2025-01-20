04f540b1f123f77ed6fcb8a747c6c87fdcc1ae01
Arka Garai <arka292@gmail.com>
2025-01-20 22:20:38 +0530

    c

--- 1)Array Stack Recursion Queue Sorting/15Insertion_sort.c
+++ 1)Array Stack Recursion Queue Sorting/15Insertion_sort.c
@@ -1,29 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-  int n,i,j,temp;
+  int n, i, j, temp;
   printf("Enter the no. of Elements: ");
-  scanf("%d",&n);
+  scanf("%d", &n);
   int arr[n];
   printf("Enter the Elements Of the Array: ");
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+  for (i = 0; i < n; i++){
+    scanf("%d", &arr[i]);
+  }
+
+  // Insertion Sort
+  for (i = 1; i <= n - 1; i++){
+    temp = arr[i];
+    j = i - 1;
+    while (j >= 0 && arr[j] > temp){
+      arr[j + 1] = arr[j];
+      j--;
     }
-  	
-  	// Insertion Sort 
-  	for(i=1;i<=n-1;i++){
-  		temp = arr[i];
-  		j=i-1;
-  		while(j>=0 && arr[j]>temp){
-  			arr[j+1]=arr[j];
-  			j--;
-		  }
-		arr[j+1]=temp;  
-  		
-	  }
-  	
-	printf("\nThe Sorted Array:\n");
-    for(i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
+    arr[j + 1] = temp;
+  }
+
+  printf("\nThe Sorted Array:\n");
+  for (i = 0; i < n; i++){
+    printf("%d ", arr[i]);
+  }
 }
