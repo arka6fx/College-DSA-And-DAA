866ab098dd1b62305ef95e1f94a06f3545a43d23
Arka Garai <arka292@gmail.com>
2025-01-20 20:35:23 +0530

    c

--- 1)Array Stack Recursion Queue Sorting/11Searching_menudriven.c
+++ 1)Array Stack Recursion Queue Sorting/11Searching_menudriven.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int linearSearch(int arr[], int n, int data)
-{
+int linearSearch(int arr[], int n, int data){
 	int i, idx;
 	for (i = 0; i <= n - 1; i++)
 	{
@@ -15,43 +14,39 @@ int linearSearch(int arr[], int n, int data)
 	printf("Element %d not found in the array\n", data);
 	exit(1);
 }
-void bubbleSort(int arr[], int n)
-{
+
+void bubbleSort(int arr[], int n){
 	int flag, temp, i, j;
-	for (i = 1; i <= n - 1; i++)
-	{
+
+	for (i = 1; i <= n - 1; i++){
+
 		flag = 0; // marks the pass where there is no swapping
-		for (j = 1; j <= n - i; j++)
-		{
-			if (arr[j - 1] > arr[j])
-			{
+		for (j = 1; j <= n - i; j++){
+			if (arr[j - 1] > arr[j]){
 				temp = arr[j - 1];
 				arr[j - 1] = arr[j];
 				arr[j] = temp;
 				flag = 1;
 			}
 		}
-		if (flag == 0)
-		{
+		if (flag == 0){
 			break;
 		}
 	}
 }
 
-int binarySearch(int arr[], int n, int data)
-{
+int binarySearch(int arr[], int n, int data){
 	bubbleSort(arr, n);
+
 	int start = 0, end = n - 1, idx, mid;
-	while (start <= end)
-	{
+
+	while (start <= end){
 		mid = start + (end - start) / 2;
-		if (arr[mid] == data)
-		{
+		if (arr[mid] == data){
 			idx = mid;
 			return idx;
 		}
-		else if (arr[mid] < data)
-		{
+		else if (arr[mid] < data){
 			start = mid + 1;
 		}
 		else
@@ -61,21 +56,18 @@ int binarySearch(int arr[], int n, int data)
 	exit(1);
 }
 
-int main()
-{
+int main(){
 	int choice, n, idx, data, i;
 	printf("Enter the number of elements: ");
 	scanf("%d", &n);
 
 	int arr[n];
 	printf("Enter the elements of the array: ");
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++){
 		scanf("%d", &arr[i]);
 	}
 
-	while (1)
-	{
+	while (1){
 
 		printf("\nChoose the search method:\n");
 		printf("1. Linear Search\n");
@@ -83,8 +75,7 @@ int main()
 		printf("3. Exit\n");
 		scanf("%d", &choice);
 
-		switch (choice)
-		{
+		switch (choice){
 		case 1:
 			printf("Enter the Element to Find: ");
 			scanf("%d", &data);
