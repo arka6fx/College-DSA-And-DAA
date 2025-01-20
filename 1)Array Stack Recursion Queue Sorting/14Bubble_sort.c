0011152514a7ab58ca7900f1ca429de567a9289d
Arka Garai <arka292@gmail.com>
2025-01-20 22:00:19 +0530

    c

--- 1)Array Stack Recursion Queue Sorting/14Bubble_sort.c
+++ 1)Array Stack Recursion Queue Sorting/14Bubble_sort.c
@@ -1,35 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-  int n,i,j,flag,temp;
-  printf("Enter the no. of Elements: ");
-  scanf("%d",&n);
-  int arr[n];
-  printf("Enter the Elements Of the Array: ");
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-  	
-  	//Bubble Sort
-  	for(i=1;i<=n-1;i++){
-  		flag=0;//marks the pass where there is no swapping
-  		for(j=1;j<=n-i;j++){
-  			if(arr[j-1]>arr[j]){
-  				temp = arr[j-1];
-  				arr[j-1] = arr[j];
-  				arr[j] = temp;
-  				flag=1;
-			  }
-			  
-		  }
-		  if(flag==0){
-		  	break;
-		  }
-  		
+	int n, i, j, flag, temp;
+	printf("Enter the no. of Elements: ");
+	scanf("%d", &n);
+	int arr[n];
+	printf("Enter the Elements Of the Array: ");
+	for (i = 0; i < n; i++){
+		scanf("%d", &arr[i]);
 	}
-	
+
+	// Bubble Sort
+	for (i = 1; i <= n - 1; i++){
+		flag = 0; // marks the pass where there is no swapping
+		for (j = 1; j <= n - i; j++){
+			if (arr[j - 1] > arr[j]){
+				temp = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = temp;
+				flag = 1;
+			}
+		}
+		if (flag == 0){
+			break;
+		}
+	}
+
 	printf("\nThe sorted Array:\n");
-    for(i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
+	for (i = 0; i < n; i++){
+		printf("%d ", arr[i]);
+	}
 }
