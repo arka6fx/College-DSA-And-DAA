c676f8c409c22e54ab10a65729c15af97b5470fe
Arka Garai <arka292@gmail.com>
2025-01-20 23:32:57 +0530

    c

--- 1)Array Stack Recursion Queue Sorting/16Selection_sort.c
+++ 1)Array Stack Recursion Queue Sorting/16Selection_sort.c
@@ -1,33 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-  int n,i,j,min,temp;
-  printf("Enter the no. of Elements: ");
-  scanf("%d",&n);
-  int arr[n];
-  printf("Enter the Elements Of the Array: ");
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-  	
-  	// Selection Sort 
-  	for(i=0;i<=n-2;i++){
-  		min=i;
-  		for(j=i+1;j<=n-1;j++){
-  			if(arr[j]<arr[min]){
-  				min=j;
-			  }
-		  }
-		if(min!=i){
+	int n, i, j, min, temp;
+	printf("Enter the no. of Elements: ");
+	scanf("%d", &n);
+	int arr[n];
+	printf("Enter the Elements Of the Array: ");
+	for (i = 0; i < n; i++){
+		scanf("%d", &arr[i]);
+	}
+
+	// Selection Sort
+	for (i = 0; i <= n - 2; i++){
+		min = i;
+		for (j = i + 1; j <= n - 1; j++){
+			if (arr[j] < arr[min]){
+				min = j;
+			}
+		}
+		if (min != i){
 			temp = arr[i];
 			arr[i] = arr[min];
 			arr[min] = temp;
-		}  
- 		
-	  }
-  	
+		}
+	}
+
 	printf("\nThe Sorted Array:\n");
-    for(i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
+	for (i = 0; i < n; i++){
+		printf("%d ", arr[i]);
+	}
 }
