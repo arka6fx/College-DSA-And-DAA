c8eb825e98060de2ee32b13676d6bdac8ec96e88
Arka Garai <arkagarai292@gmail.com>
2025-03-09 12:35:12 +0530

    c

--- 6)DAA/06FloydWarshall.c
+++ 6)DAA/06FloydWarshall.c
@@ -1,14 +1,14 @@
 // 04/03/25
 #include <stdio.h>
-#include <limits.h>
 #define V 4
+#define INF 99999
 
 void printSolution(int dist[][V]) {
-    int i, j;
+	int i,j;
     printf("The following matrix shows the all pairs of shortest path: \n");
-    for (i = 0; i < V; i++) {
-        for (j = 0; j < V; j++) {
-            if (dist[i][j] == INT_MAX)
+    for ( i = 0; i < V; i++) {
+        for ( j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
                 printf("%7s", "INF");
             else
                 printf("%7d", dist[i][j]);
@@ -23,7 +23,7 @@ void floydWarshall(int dist[][V]) {
     for (k = 0; k < V; k++) {
         for (i = 0; i < V; i++) {
             for (j = 0; j < V; j++) {
-                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j])
+                if (dist[i][k] + dist[k][j] < dist[i][j])
                     dist[i][j] = dist[i][k] + dist[k][j];
             }
         }
@@ -32,12 +32,14 @@ void floydWarshall(int dist[][V]) {
     printSolution(dist);
 }
 
+
 int main() {
-    int graph[V][V] = { { 0, 8, INT_MAX, 1 },
-                        { INT_MAX, 0, 1, INT_MAX },
-                        { 4, INT_MAX, 0, INT_MAX },
-                        { INT_MAX, 2, 9, 0 } };
+    int graph[V][V] = { { 0,8, INF, 1 },
+                        { INF, 0, 1, INF },
+                        { 4, INF, 0, INF },
+                        { INF, 2, 9, 0 } };
 
     floydWarshall(graph);
     return 0;
 }
+
